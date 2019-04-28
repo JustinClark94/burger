e229b4db9945df10617206d2f921b1d1ff4d9fc7
Justin Clark <justclark.jose@gmail.com>
2019-04-28 14:43:03 -0500

    submission only. not final

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Bacon');
+INSERT INTO burgers (name) VALUES ('Bacon', false);
 INSERT INTO burgers (name) VALUES ('Chicken');
 INSERT INTO burgers (name) VALUES ('Black Bean');
